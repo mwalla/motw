52207e342976848958e38a94c56aaa47c5c1b531
llpk79 <paul.kutrich@slalom.com>
2020-12-30 23:40:00 -0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='character_tracker',
+    version='0.1',
+    packages=['character_tracker'],
+    url='https://github.com/llpk79/motw',
+    license='MIT',
+    author='Paul Kutrich',
+    author_email='pkutrich@gmail.com',
+    description='Tracker for Moster of the Week characters',
+    long_description=long_description
+)
