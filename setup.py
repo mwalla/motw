f334f4cae25abd5f5d9f926c3050fcf40ef7e435
llpk79 <paul.kutrich@slalom.com>
2021-01-02 10:39:48 -0800

    Version 0.10, typo, update package_data.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='character_tracker',
-    version='0.9',
-    packages=['character_tracker'],
+    version='0.10',
+    packages=setuptools.find_packages(),
     url='https://github.com/llpk79/motw',
     license='MIT',
     author='Paul Kutrich',
     author_email='pkutrich@gmail.com',
-    description='Tracker for Moster of the Week characters',
+    description='Tracker for Monster of the Week characters',
     long_description=long_description,
     include_package_data=True,
     package_data={
-        "character_tracker": ["characters/*.pkl", "archetypes/*.json"]
+        "character_tracker": ["pickle/*.pkl", "archetypes/*.json"]
     }
 )
